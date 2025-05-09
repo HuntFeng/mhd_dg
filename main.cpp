d5653419a1b90f8cd49ff4cd01bcb7b571095f41
HuntFeng <spacecurvature@gmail.com>
2025-05-09 17:00:45 -0600

    update

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <Kokkos_Core.hpp>
 #include <fstream>
+#include <gridformat/gridformat.hpp>
 
 using Kokkos::MDRangePolicy;
 using Kokkos::parallel_for;
 using Kokkos::printf;
 using Kokkos::View;
+const double pi = Kokkos::numbers::pi;
 
-// gcc version too old because system is too old?
-// use ubunty instead of conda image, but we need to install python properly
-void save(const View<double**>& u, const View<double**>& mesh_x,
+void save(const View<double***>& u, const View<double**>& mesh_x,
           const View<double**>& mesh_y, int step) {
     std::fstream fs;
     fs.open(std::format("data/step_{:03d}.csv", step), std::fstream::out);
     fs << "x, y, u\n";
     for (int i = 0; i < u.extent(0); ++i) {
         for (int j = 0; j < u.extent(1); ++j) {
+            printf("u(%d,%d, 1)=%f\n", i, j, u(i, j, 1));
             fs << std::format("{}, {}, {}\n", mesh_x(i, j), mesh_y(i, j),
-                              u(i, j));
+                              u(i, j, 1));
         }
     }
 }
@@ -25,10 +26,16 @@ int main(int argc, char* argv[]) {
     Kokkos::ScopeGuard guard(argc, argv);
     // prepare mesh
     int nx = 32, ny = 32;
-    std::array<double, 2> x_range{0, 2 * Kokkos::numbers::pi};
-    std::array<double, 2> y_range{0, 2 * Kokkos::numbers::pi};
+    std::array<double, 2> x_range{0, 2 * pi};
+    std::array<double, 2> y_range{0, 2 * pi};
     View<double**> mesh_x("mesh_x", nx, ny);
     View<double**> mesh_y("mesh_y", nx, ny);
+
+    GridFormat::ImageGrid<2, double> grid{
+        {2 * pi, 2 * pi}, // domain size
+        {32, 32}          // number of cells (pixels) in each direction
+    };
+    GridFormat::VTKHDFTimeSeriesWriter writer{grid, "data"};
     parallel_for(
         MDRangePolicy({0, 0}, {nx, ny}), KOKKOS_LAMBDA(int i, int j) {
             mesh_x(i, j) =
@@ -52,5 +59,16 @@ int main(int argc, char* argv[]) {
             u(i, j, 4) = Kokkos::sin(2 * mesh_x(i, j));
             u(i, j, 5) = gamma;
         });
-    return 0;
+
+    // save initial condition
+    // save(u, mesh_x, mesh_y, 0);
+    writer.set_cell_field("mx", [&](const auto cell) {
+        return u(cell.location[0], cell.location[1], 1);
+    });
+    writer.write(0.0);
+
+    writer.set_cell_field("mx", [&](const auto cell) {
+        return u(cell.location[0], cell.location[1], 1);
+    });
+    writer.write(0.1);
 }
